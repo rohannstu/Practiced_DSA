cfbf8aed772ddb2e7cd03b9371406f46ab9633ff
rohannstu <rohansinha141635@gmail.com>
2024-10-27 22:04:31 +0600

    Optimal soluion

---
+++ Array/MissingNumber.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0, N = 0;
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(nums[i] != n){
+                sum += nums[i];
+            }
+            else{
+                N = 1;
+            }
+        }
+        int indx_sum;
+        int ans = 0;
+        indx_sum = (((n - 1) * (n)) / 2);
+        if((indx_sum - sum) != 0){
+            ans = indx_sum - sum;
+        }
+        else{
+            if(N == 1){
+                ans = 0;
+            }
+            else{
+                ans = n;
+            }
+        }
+        return ans;
+    }
+}
